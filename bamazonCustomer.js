1844e463f52c814a12feda37d54c8ce5b2923096
Wood Jerry Remelus <woodjremelus@gmail.com>
2018-04-19 01:24:22 -0600

    homework

---
+++ bamazonCustomer.js
@@ -0,0 +1,99 @@
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    displayInventory();
+});
+
+// displayInventory will retrieve the current inventory from the database and output it to the console
+function displayInventory() {
+	console.log('___In Stores___');
+
+    var query = connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+            console.log("-----------------------------------");
+        }
+        offerCustomer();
+    });
+}
+
+// promptUserPurchase will prompt the user for the item/quantity they would like to purchase
+function offerCustomer() {
+
+	// Prompt the user to select an item
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			filter: Number
+		}
+	]).then(function(input) {
+		
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		// Query db to confirm that the given item ID exists in the desired quantity
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0) {
+				console.log('Please enter a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				// If the quantity requested by the user is in stock
+				if (quantity <= productData.stock_quantity) {
+					console.log('Placing order!');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+ 
+						// End the database connection
+						connection.end();
+					})
+				} else {
+					console.log('Sorry! low inventory, We can not place your order.');
+					console.log('Would you like to buy less or something else?');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
